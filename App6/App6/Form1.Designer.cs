6fdbcd427ac644b7bba843354c3d06a22ac0d4f3
tofanijo <tofanijo@gmail.com>
2020-01-14 10:48:00 +0200

    App6

---
+++ App6/App6/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace App6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(26, 112);
+            this.tbInput.Multiline = true;
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(162, 54);
+            this.tbInput.TabIndex = 0;
+            this.tbInput.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(205, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "spausti";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.Location = new System.Drawing.Point(299, 112);
+            this.tbOutput.Multiline = true;
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.Size = new System.Drawing.Size(188, 178);
+            this.tbOutput.TabIndex = 2;
+            this.tbOutput.TextChanged += new System.EventHandler(this.tbOutput_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 183);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Iveskite temperratura C";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbOutput);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbOutput;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ App6/App6/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double tempC;
+        double tempK;
+        double tempF;
+        double tempR;
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //temperaturos skaiciuokle
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(!double.TryParse(tbInput.Text, out tempC))
+            {
+                MessageBox.Show("Ivesti neteisingi duomenis");
+                return;
+            }
+            tempC = double.Parse(tbInput.Text);
+            tempK = tempC + 273.16;
+            tempF = tempC * 9 / 5 + 32;
+            tempR = tempC * 0.8;
+
+            tbOutput.Clear();
+            tbOutput.AppendText($"Tempertura\r\n");
+            tbOutput.AppendText($"Celsijais: {tempC}\r\n");
+            tbOutput.AppendText($"Kelvinais: {tempK}\r\n");
+            tbOutput.AppendText($"Fahrenheitais: {tempF}\r\n");
+            tbOutput.AppendText($"Reomiurais: {tempR}\r\n");
+
+        }
+
+        private void tbOutput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
